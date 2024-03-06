4f72400b1e39a57f57fd07d4eeb833bc8d80de0a
Карташов Дмитрий Дмитриевич <ddkartashov@vtb.ru>
2024-03-06 15:20:14 +0300

    init commit

---
+++ src/main/java/com/example/cfttojavaex4/CftToJavaEx4Application.java
@@ -0,0 +1,13 @@
+package com.example.cfttojavaex4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CftToJavaEx4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CftToJavaEx4Application.class, args);
+	}
+
+}
